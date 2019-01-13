4970c06c9f845112402fc76fc56b31209bf5d6b6
Ruslan Kornev <oganer@gmail.com>
2019-01-14 00:43:15 +0300

    Adds bages and write OUTPUT.md

---
+++ adds_bages.js
@@ -0,0 +1,28 @@
+var os = require("os");
+const fs = require('fs');
+const fsPromises = fs.promises;
+const readline = require('readline');
+
+(async () => {
+  const fileStream = fs.createReadStream('README.md');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  outfile = await fsPromises.open("OUTPUT.md", "w");
+
+  for await (const line of rl) {
+    let m = line.match(/https:\/\/github.com\/(\S+?)\/(\S+?)[\/)]/);
+    let bage;
+
+    if (m && m[1] && m[2]) {
+      bage = ` ![](https://img.shields.io/github/stars/${m[1]}/${m[2]}.svg?label=Stars&style=social?style=popout)`  
+    } else {
+      bage = ''
+    }
+
+    await fsPromises.writeFile(outfile, line + bage + os.EOL);
+  }
+})()
